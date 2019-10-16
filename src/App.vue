1d2e1f3fa8497b8711d44ede84084d43cb3b54c1
Grig Alex <alexengrigdev@gmail.com>
2019-10-16 21:05:16 +0300

    Adds Toolbar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <Toolbar />
     </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue';
+  import Toolbar from './components/Toolbar';
 
   export default {
     name: 'app',
     components: {
-      HelloWorld
+      Toolbar
     }
   };
 </script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="page-container">
+        <md-app md-mode="fixed-last" md-waterfall>
+            <md-app-toolbar class="md-large md-dense md-primary">
+                <div class="md-toolbar-row">
+                    <div class="md-toolbar-section-start">
+                        <md-button @click="menuVisible = !menuVisible" class="md-icon-button">
+                            <md-icon>menu</md-icon>
+                        </md-button>
+
+                        <span class="md-title">My Title</span>
+                    </div>
+                    <div class="md-toolbar-section-end">
+                        <md-button class="md-icon-button">
+                            <md-icon>more_vert</md-icon>
+                        </md-button>
+                    </div>
+                </div>
+                <div class="md-toolbar-row">
+                    <md-tabs class="md-primary">
+                        <md-tab id="tab-home" md-label="Home"></md-tab>
+                        <md-tab id="tab-pages" md-label="Pages"></md-tab>
+                        <md-tab id="tab-posts" md-label="Posts"></md-tab>
+                        <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+                    </md-tabs>
+                </div>
+            </md-app-toolbar>
+            <md-app-drawer :md-active.sync="menuVisible">
+                <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+                <md-list>
+                    <md-list-item>
+                        <md-icon>move_to_inbox</md-icon>
+                        <span class="md-list-item-text">Inbox</span>
+                    </md-list-item>
+                    <md-list-item>
+                        <md-icon>send</md-icon>
+                        <span class="md-list-item-text">Sent Mail</span>
+                    </md-list-item>
+                    <md-list-item>
+                        <md-icon>delete</md-icon>
+                        <span class="md-list-item-text">Trash</span>
+                    </md-list-item>
+                    <md-list-item>
+                        <md-icon>error</md-icon>
+                        <span class="md-list-item-text">Spam</span>
+                    </md-list-item>
+                </md-list>
+            </md-app-drawer>
+            <md-app-content>
+                Test
+            </md-app-content>
+        </md-app>
+    </div>
+</template>
+
+<style scoped>
+    .md-app {
+        max-height: 400px;
+        border: 1px solid blue;
+    }
+
+    .md-drawer {
+        width: 230px;
+        max-width: calc(100vw - 125px);
+    }
+</style>
+
+<script>
+  export default {
+    name: 'LastRowFixed',
+    data: () => ({
+      menuVisible: false
+    })
+  };
+</script>
