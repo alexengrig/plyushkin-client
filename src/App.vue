274781ebfb22186246010838e93af3ac97858927
Grig Alex <alexengrigdev@gmail.com>
2019-10-21 21:56:27 +0300

    Adds simple todo list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
     <div id="app">
-        <Toolbar />
+        <label for="input">Text: </label>
+        <input @keypress.enter="onAddText" id="input" v-model="text" />
+        <ul>
+            <li @dblclick="onDeleteText(index)"
+                v-bind:key="index"
+                v-for="(item, index) in items"
+            >
+                {{ item }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-  import Toolbar from './components/Toolbar';
-
   export default {
     name: 'app',
-    components: {
-      Toolbar
+    data() {
+      return {
+        text: 'empty',
+        items: [
+          'Item 1',
+          'Item 2'
+        ]
+      };
+    },
+    methods: {
+      onAddText() {
+        this.items.push(this.text);
+      },
+      onDeleteText(index) {
+        this.items.splice(index, 1);
+      }
     }
   };
 </script>
